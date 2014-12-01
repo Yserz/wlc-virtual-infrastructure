c15ae90ce2fd5a9b149dcf66b01dd6aafb7ce3b6
Yserz <michael.koppen@googlemail.com>
2014-12-01 17:55:53 +0100

    seperated wlc and chef jsons

--- pull_subs.sh
+++ pull_subs.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+# git fetch wlc-boxes master
 git subtree pull --squash --prefix wlc-boxes wlc-boxes master
+# git fetch wlc-chef-repo master
 git subtree pull --squash --prefix virtual-chef-server/chef-repo wlc-chef-repo master
