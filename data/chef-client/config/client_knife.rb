8840efd09a4b5464071b8cccec2ed026ef19c876
Yserz <michael.koppen@googlemail.com>
2014-12-25 04:01:15 +0100

    exchanged shell provisioners with chef cookbooks

--- data/chef-client/config/client_knife.rb
+++
@@ -1,7 +0,0 @@
-log_level                :info
-log_location             STDOUT
-client_key               '/etc/chef/client.pem'
-chef_server_url          'https://192.168.1.2:9191/'
-ssl_verify_mode          :verify_none
-syntax_check_cache_path  '/home/vagrant/.chef/syntax_check_cache'
-cookbook_path [ '/vagrant/wlc-chef-repo/cookbooks' ]
